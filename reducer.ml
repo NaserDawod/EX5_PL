1acd37c4fc709a96a0e904e03b9e7069729adf42
NaserDawod <90802273+NaserDawod@users.noreply.github.com>
2023-01-04 19:37:11 +0200

    Add files via upload

--- reducer.ml
+++ reducer.ml
@@ -29,29 +29,36 @@ let rec variables = function
     | Application (t1,t2) -> StringSet.union (variables t1) (variables t2);;
 
 let rec substitute x t1 t2 = match t2  with
-										| Variable y -> if x = y then t1 else t2
-										| Abstraction (y, t) -> if x = y then t2
-																								else if not (StringSet.mem y (fv t1)) then Abstraction(y, substitute x t1 t)
-																						    else let z = fresh_var (variables t) in let t_tag = substitute y (Variable(z)) t
-																										 in (Abstraction(z, substitute x t1 t_tag))
-										| Application(t11,t22) -> Application(substitute x t1 t11, substitute x t1 t22);;
+		| Variable y -> if x = y then t1 else t2
+		| Abstraction (y, t) -> if x = y then t2
+																else if not (StringSet.mem y (fv t1)) then Abstraction(y, substitute x t1 t)
+																else let z = fresh_var (fv t1) in let t_tag = substitute y (Variable(z)) t
+																			in (Abstraction(z, substitute x t1 t_tag))
+		| Application(t11,t22) -> Application(substitute x t1 t11, substitute x t1 t22);;
 
 let get_val opt = match opt with
-                | None -> raise (SyntaxError "Fuck.\n")
-                | Some x -> x;;	
+		| None -> raise (SyntaxError "Error.\n")
+		| Some x -> x;;	
 
-let reduce_cbv t = match t with
-										| Application (Abstraction (x, e1), Application(f,f2)) -> None
-										| Application (Abstraction (x, e1), s) -> Some (substitute x s e1)
-										| _ -> None;;
+let rec reduce_cbv t = match t with
+		| Abstraction (x, t1) -> None
+		| Application (t1, Application (t21, t22)) -> let t2 = Application (t21, t22) in 
+		  Some (Application (t1, get_val (reduce_cbv t2)))
+		| Application (Abstraction (x, t1), s) -> Some (substitute x s t1)
+		| Application (Application (t11, t12), s) -> let t' = Application (t11, t12) in 
+			Some (Application (get_val (reduce_cbv t'), s))
+		| _ -> None;;
 
 let rec reduce_cbn t = match t with
-										| Application (Abstraction (x, e1), s) -> Some (substitute x s e1)
-										| Application (Application (t1, t2), s) -> let t_tag = Application(t1, t2) in
-											Some (Application (get_val (reduce_cbn t_tag), s))
-										| _ -> None;;		
+		| Abstraction (x, t1) -> None
+		| Application (Abstraction (x, e1), s) -> Some (substitute x s e1)
+		| Application (Application (t1, t2), s) -> let t' = Application (t1, t2) in
+			Some (Application (get_val (reduce_cbn t'), s))
+		| _ -> None;;
 
 let rec evaluate verbose func t = if verbose then (print_string t); 
-	 										(let t_tag = (func t) in match t_tag with
-																| None -> ()
-																| _ -> evaluate verbose func (get_val t_tag));;
+			(let t_tag = (func t) in match t_tag with
+								| None -> ()
+								| _ -> evaluate verbose func (get_val t_tag));;
+
+
--- tests.ml
+++ tests.ml
@@ -41,23 +41,54 @@ let s3 = "
 ((\\id1.(t1 id1)) (\\id2.(t1 t2)))
 ";;
 
-(* (((\b.(\c.((b c) (\t.(\f.f))))) (\t.(\f.t))) (\t.(\f.t))) *)
+let s4 = "
+let id = (\\x.(x)) in
+(id (id (\\z.(id z))))
+";;
 
-let () =
-  printf "\nEvaluating:\n%s\nin cbn semantics:\n\n" s1;
-  ignore (evaluate ~verbose:true reduce_cbn (parse s1));
-  printf "\n\nEvaluating:\n%s\nin cbv semantics:\n\n" s2;
-  ignore (evaluate ~verbose:true reduce_cbv (parse s2));
+let s5 = "let test = (\\l.(\\m.(\\n. ((l m) n)))) in
+let tru = (\\t.(\\f.t)) in
+let fls = (\\t.(\\f.f)) in
+(((test tru) (\\y.y)) (\\z.z))";;
 
-  printf "\n\n Testing on:\n%s\nReduce cbv\n\n" s3;
-  ignore (evaluate ~verbose:true reduce_cbv (parse s3));
-  printf "\n\n Testing on:\n%s\nReduce cbn\n\n" s3;
-  ignore (evaluate ~verbose:true reduce_cbn (parse s3));
-  
-  (*Format Term Conv Tests*)
-  
-  printf "%s\n" (format_term_conv(parse("(\\t.(\\f.t))" )));
-  printf "%s\n" (format_term_conv(parse("(t1 (t2 t3))")));
-  printf "%s\n" (format_term_conv(parse("((t1 t2) t3)")));
-  printf "%s\n" (format_term_conv(parse(s1)));
-  printf "%s\n" (format_term_conv(parse("(\\b.(\\c. ((b (not c)) c)))")));
+let s6 = "let id = (\\x.(x)) in
+let omega = ((\\x. (x x)) (\\x. (x x))) in
+((\\x. (\\y. (x))) (id omega))";;
+
+printf "\nCall By Value Tests\n\n";
+
+printf "\nEvaluating:\n%s\nin cbv semantics:\n\n" s1;
+ignore (evaluate ~verbose:true reduce_cbv (parse s1));
+
+printf "\n\nEvaluating:\n%s\nin cbv semantics:\n\n" s2;
+ignore (evaluate ~verbose:true reduce_cbv (parse s2));
+
+printf "\n\n nEvaluating on:\n%s\nReduce cbv\n\n" s3;
+ignore (evaluate ~verbose:true reduce_cbv (parse s3));
+
+printf "\nEvaluating:\n%s\nin cbv semantics:\n\n" s4;
+ignore (evaluate ~verbose:true reduce_cbv (parse s4));;
+
+printf "\nEvaluating:\n%s\nin cbv semantics:\n\n" s5;
+ignore (evaluate ~verbose:true reduce_cbv (parse s5));;
+
+
+printf "\nCall By Name Tests\n\n";
+
+printf "\nEvaluating:\n%s\nin cbn semantics:\n\n" s1;
+ignore (evaluate ~verbose:true reduce_cbn (parse s1));
+
+printf "\n\nEvaluating:\n%s\nin cbn semantics:\n\n" s2;
+ignore (evaluate ~verbose:true reduce_cbn (parse s2));
+
+printf "\n\n Testing on:\n%s\nReduce cbn\n\n" s3;
+ignore (evaluate ~verbose:true reduce_cbn (parse s3));
+
+printf "\nEvaluating:\n%s\nin cbn semantics:\n\n" s4;
+ignore (evaluate ~verbose:true reduce_cbn (parse s4));;
+
+printf "\nEvaluating:\n%s\nin cbn semantics:\n\n" s5;
+ignore (evaluate ~verbose:true reduce_cbn (parse s5));;
+
+printf "\nEvaluating:\n%s\nin cbn semantics:\n\n" s6;
+ignore (evaluate ~verbose:true reduce_cbn (parse s6));;
